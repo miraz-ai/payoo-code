5c5b355ba78b71994304d1d39f3ec5079d3709df
Muzan <afzalhossain.miraz@gmail.com>
2026-03-03 03:00:39 +0600

    login page is done

---
+++ script/login.js
@@ -0,0 +1,19 @@
+document.getElementById('btn-login').addEventListener('click',function(){
+   const mobileNumberInput = document.getElementById('input-number')
+    const contact = mobileNumberInput.value;
+    console.log(contact);
+
+
+    const pinInput = document.getElementById('input-pin');
+    const pin = pinInput.value;
+    console.log(pin);
+
+    if(contact == "01783203317"  && pin == "1234"){
+        alert("login Success");
+        window.location.assign("/home.html")
+    }
+    else{
+        alert("Login Failed");
+        
+    }
+});
